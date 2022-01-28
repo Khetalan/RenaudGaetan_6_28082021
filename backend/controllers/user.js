ac85888d5229ce8fa7ea9a7e994648c93e75294c
Gaëtan RENAUD <gaetan.renaud26@gmail.com>
2022-01-28 13:12:58 +0100

    add-dotenv/passwdValidator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,37 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const passwordValidator = require('password-validator');
+
+var schema = new passwordValidator();
+
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123', 'admin']); // Blacklist these values
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message : 'Utilisateur Crée !'}))
-                .catch(error=> res.status(400).json({error}));
-        })
-        .catch(error => res.startus(500).json({error}));
+    if(schema.validate(req.body.password)){
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User ({
+                    email: req.body.email,
+                    password: hash
+                })
+                user.save()
+                    .then(()=>res.status(201).json({message: 'Utilisateur créé !'}))
+                    .catch(error=>(res.status(401).json({error})));
+                })
+            .catch(error=>(res.status(500).json({error})));
+    }else {
+        return res.json({message: 'Votre mot de passe ne contient les critères demandé...'});
+    } 
 };
 
 exports.login = ('',(req, res, next) => {
@@ -31,8 +49,8 @@ exports.login = ('',(req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId : user._id},
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn : '24h'}
+                            process.env.secretToken,
+                            { expiresIn : process.env.secretExpiration}
                         )
                     });
                 })
